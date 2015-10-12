33209480ea18469008a2fffe7dd1764dfc715f52
YIMINGXU <100650920@qq.com>
2015-10-12 10:07:55 +0800

    frist to yarn

---
+++ src/main/java/com/xym/hadoop/asyncdispatcher/InitTransition.java
@@ -0,0 +1,5 @@
+package com.xym.hadoop.asyncdispatcher;
+
+import org.apache.hadoop.yarn.state.SingleArcTransition;
+
+
---
+++ src/main/java/com/xym/hadoop/asyncdispatcher/JobStateInternal.java
@@ -0,0 +1,10 @@
+package com.xym.hadoop.asyncdispatcher;
+  public enum JobStateInternal { //作业内部状态  
+	CREATE,
+    NEW,  
+    SETUP,  
+    INITED,  
+    RUNNING,  
+    SUCCEEDED,  
+    KILLED,  
+  }  
